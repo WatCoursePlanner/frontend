5a0f22bc0f01da618c33b5be982b130379b5f656
CarbonylGroup <carbonylgp@gmail.com>
2020-07-08 14:34:11 -0400

    redux folders restructure

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from "react-redux";
 import {createStore} from 'redux'
-import rootReducer from './redux/reducers'
+import rootReducer from './reducers'
 import App from './components/App';
 import * as serviceWorker from './serviceWorker';
 import './index.scss';
