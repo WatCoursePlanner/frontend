bb798763e9c9347b11b9fe13933d9bb6313e9c4b
ZonaHu <hzm5622@126.com>
2020-07-09 15:23:17 -0400

    Find your courses

--- src/components/DiscoverPage.js
+++ src/components/DiscoverPage.js
@@ -1,19 +1,13 @@
 import React from 'react'
-import {Link} from "react-router-dom";
-import WhatshotIcon from '@material-ui/icons/Whatshot';
 import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import '../index.scss';
 
 const DiscoverPage = () => {
-    {/* TODO: Adding Filters */}
+    /* TODO: Adding Filters */
 
     return(
         <div>
         <div style={{
-            width: '100%',
-            height: '8%',
-            fontFamily: 'Lato',
-            fontWeight: 700,
             paddingLeft: 60,
         }}>
             <br/><h3>Find Your Courses</h3>
