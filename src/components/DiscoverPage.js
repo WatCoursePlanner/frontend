d7fffd88500193eae1b29c64154333828e397af9
ZonaHu <hzm5622@126.com>
2020-07-09 01:00:53 -0400

    Update To-Do and Icon for More Courses

--- src/components/DiscoverPage.js
+++ src/components/DiscoverPage.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import {Link} from "react-router-dom";
 import WhatshotIcon from '@material-ui/icons/Whatshot';
+import NavigateNextIcon from '@material-ui/icons/NavigateNext';
 import '../index.scss';
 
 const DiscoverPage = () => {
+    {/* July 9 TODO: Adjusting the Postion for Popular Courses */}
+    {/* TODO: Adding Course Cards/Navigate Next Icon */}
 
     return(
+        <div>
         <div style={{
             width: '100%',
             height: '8%',
@@ -21,6 +25,14 @@ const DiscoverPage = () => {
              Popular Courses
             </span>
         </div>
+
+        <div style={{
+            paddingLeft: 60,
+        }}>
+        <NavigateNextIcon className={'more_courses'}>
+        </NavigateNextIcon>
+        </div>
+    </div>
     )
 }
 
