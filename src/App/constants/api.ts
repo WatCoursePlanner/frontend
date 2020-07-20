6fece133090acb8fe33e882996dbe5d7c1d7e300
CarbonylGroup <carbonylgp@gmail.com>
2020-07-20 00:38:22 -0400

    Update api URL

--- src/App/constants/api.ts
+++ src/App/constants/api.ts
@@ -1,2 +1,2 @@
-// export const URL_BASE = 'https://watcourses.com/api'
-export const URL_BASE = 'http://localhost:8080'
+export const URL_BASE = 'https://watcourses.com/api'
+// export const URL_BASE = 'http://localhost:8080'
