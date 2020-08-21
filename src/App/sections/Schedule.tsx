dc30ca7276d2c399313990b4df8201613c6522fb
CarbonylGroup <carbonylgp@gmail.com>
2020-08-21 15:49:47 -0400

    chip ripple mask for safari

--- src/App/sections/Schedule.tsx
+++ src/App/sections/Schedule.tsx
@@ -18,7 +18,7 @@ import {URL_BASE} from "../constants/api";
 import {CheckResults, FindSlotRequest} from "../proto/courses";
 
 const ShortListButton = styled(Button)<ButtonProps & ButtonHTMLProps>`
-position:absolute;
+  position:absolute;
   top: 50%;
   right: 0;
   margin-top: -28px;
