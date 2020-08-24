62b8fd4b50f91cb2446e45e6140f3d31995f6501
CarbonylGroup <carbonylgp@gmail.com>
2020-08-24 11:10:30 -0400

    hook callback type

--- src/App/hooks/useDetectClickOutside.ts
+++ src/App/hooks/useDetectClickOutside.ts
@@ -1,6 +1,6 @@
 import React, {useEffect} from "react";
 
-const useDetectClickOutside = (ref: React.MutableRefObject<any>, callback: () => void) => {
+const useDetectClickOutside = (ref: React.MutableRefObject<any>, callback: () => any) => {
     useEffect(() => {
         function handleClickOutside(event: any) {
             if (ref.current && !ref.current.contains(event.target)) {
