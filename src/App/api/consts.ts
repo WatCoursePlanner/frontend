3ae4bd5b45c8dff26494bf2bb4de36f4b3bc8294
Mark Wang <carbonylgp@gmail.com>
2021-02-20 18:06:40 -0500

    fix API endpoint URL

--- src/App/api/consts.ts
+++ src/App/api/consts.ts
@@ -1,5 +1,5 @@
-// export const URL_BASE = 'https://watcourses.com/api'
-export const URL_BASE = 'http://localhost:8080'
+export const URL_BASE = 'https://watcourses.com/api'
+// export const URL_BASE = 'http://localhost:8080'
 export const COURSES_INFO_REFRESH_TIME = 24 * 60 * 60 * 1000 // in millis
 
 export const STUDENT_PROFILE = "/profile"
