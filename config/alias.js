b87feeb02320f863a6602c79d2234aafe7f30ad8
Harry Yu <harryyunull@gmail.com>
2022-08-16 13:44:03 -0400

    fix singletons

--- config/alias.js
+++
@@ -1,8 +0,0 @@
-const path = require("path");
-const fs = require("fs");
-const appDirectory = fs.realpathSync(process.cwd());
-
-// Update mappings in tsconfig.json as well.
-module.exports = {
-    "@watcourses": path.resolve(path.resolve(appDirectory, "src"), "./App"),
-};
--- config/webpack.prod.conf.js
+++
@@ -1,7 +0,0 @@
-const { merge } = require('webpack-merge')
-const baseConfig = require('./webpack.base.conf.js');
-
-console.log(__dirname);
-module.exports = merge(baseConfig, {
-    mode: 'production'
-});
